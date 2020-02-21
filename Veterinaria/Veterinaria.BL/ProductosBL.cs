d1afa936abdb574e86d8296964f6be2723ba9bcf
Alba Gomez <Alba Gomez@DESKTOP-2BUJ0MF>
2020-02-20 18:34:09 -0600

    Agregando entity framewrk code first

---
+++ Veterinaria/Veterinaria.BL/ProductosBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.BL
+{
+    public class ProductosBL
+    {
+        Contexto _contexto;
+
+        public List<Productos> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Productos>();
+
+        }
+        public List<Productos> ObtenerProductos()
+        {
+                  
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
+        }
+    }
+}
